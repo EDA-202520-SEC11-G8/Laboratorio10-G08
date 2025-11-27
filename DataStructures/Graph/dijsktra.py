a902d5a967d559c942a3f5a69d2c1c8053422e4b
Dantinja <d.tinjaca@unianes.edu.com>
2025-11-27 18:02:24 -0500

    creo que funciona mejor esta vez

--- DataStructures/Graph/dijsktra.py
+++ DataStructures/Graph/dijsktra.py
@@ -1,9 +1,10 @@
-from DataStructures.Map import map_linear_probing as mp
-from DataStructures.Map import priority_queue as pq
-from DataStructures.Stack import stack
-from DataStructures.Graph import digraph as G
-from DataStructures.Graph.dijsktra_structure import new_dijsktra_structure
 import math
+from DataStructures.Map import priority_queue as pq
+from DataStructures.Graph import digraph as dg
+from DataStructures.Map import map_linear_probing as map
+from DataStructures.Stack import stack as stack
+from DataStructures.Graph import dijsktra_structure as dijsktra_structure
+from DataStructures.List import array_list as lt
 
 
 def dijkstra(my_graph, source):
@@ -11,100 +12,88 @@ def dijkstra(my_graph, source):
     Ejecuta el algoritmo de Dijkstra desde un vertice origen.
     Encuentra los caminos más cortos desde source a todos los demás vertices.
     """
-    # Crear la estructura usando la función existente
-    dijkstra_struct = new_dijsktra_structure(source, G.order(my_graph))
-    
-    # Insertar el origen con distancia 0
-    mp.put(dijkstra_struct["visited"], source, {
-        "dist_to": 0,
-        "edge_to": None
-    })
-    pq.insert(dijkstra_struct["pq"], 0, source)
-    
-    # Mientras la cola de prioridad no esté vacía
-    while not pq.is_empty(dijkstra_struct["pq"]):
-        # Obtener el vértice con menor distancia
-        min_vertex = pq.remove(dijkstra_struct["pq"])
-        
-        # Obtener la información del vértice
-        vertex_info = mp.get(dijkstra_struct["visited"], min_vertex)
-        dist_v = vertex_info["dist_to"]
-        
-        # Relajar los arcos adyacentes
-        edges = G.edges_vertex(my_graph, min_vertex)
-        i = 0
-        while i < edges["size"]:
+    search = dijsktra_structure.new_dijsktra_structure(source, dg.order(my_graph))
+    visited = search["visited"]
+    heap = search["pq"]
+
+    vertices = dg.vertices(my_graph)
+
+    for i in range(lt.size(vertices)):
+        v = lt.get_element(vertices, i)
+        map.put(visited, v, {
+            "marked": False,
+            "dist_to": math.inf,
+            "edge_from": None
+        })
+
+    v_info = map.get(visited, source)
+    v_info["dist_to"] = 0
+    map.put(visited, source, v_info)
+
+    pq.insert(heap, 0, source)
+
+    while not pq.is_empty(heap):
+
+        dist_u, u = pq.remove(heap)
+        u_info = map.get(visited, u)
+
+        if u_info["marked"]:
+            continue
+
+        u_info["marked"] = True
+        map.put(visited, u, u_info)
+
+        edges = dg.edges_vertex(my_graph, u)
+
+        for i in range(edges["size"]):
             edge = edges["elements"][i]
             w = edge["vertexB"]
             weight = edge["weight"]
-            
-            # Calcular nueva distancia
-            new_dist = dist_v + weight
-            
-            # Verificar si w ya fue visitado
-            w_info = mp.get(dijkstra_struct["visited"], w)
-            
-            if w_info is None:
-                # Primera vez que visitamos w
-                mp.put(dijkstra_struct["visited"], w, {
-                    "dist_to": new_dist,
-                    "edge_to": edge
-                })
-                pq.insert(dijkstra_struct["pq"], new_dist, w)
-            else:
-                if new_dist < w_info["dist_to"]:
-                    # Encontramos un camino más corto a w
-                    w_info["dist_to"] = new_dist
-                    w_info["edge_to"] = edge
-                    mp.put(dijkstra_struct["visited"], w, w_info)
-                    # Mejorar prioridad en la cola
-                    pq.improve_priority(dijkstra_struct["pq"], new_dist, w)
-            
-            i = i + 1
-    
-    return dijkstra_struct
+
+            w_info = map.get(visited, w)
+
+            if w_info["marked"]:
+                continue
+
+            new_dist = u_info["dist_to"] + weight
+
+            if new_dist < w_info["dist_to"]:
+                w_info["dist_to"] = new_dist
+                w_info["edge_from"] = edge
+                map.put(visited, w, w_info)
+
+                if pq.contains(heap, w):
+                    pq.improve_priority(heap, new_dist, w)
+                else:
+                    pq.insert(heap, new_dist, w)
+
+    return search
 
 
-def has_path_to(dijkstra_struct, vertex):
-    """
-    Retorna True si existe un camino desde el origen hasta vertex.
-    """
-    return mp.contains(dijkstra_struct["visited"], vertex)
+def has_path_to(search, vertex):
+    v_info = map.get(search["visited"], vertex)
+    return v_info is not None and v_info["dist_to"] < math.inf
 
 
-def dist_to(dijkstra_struct, vertex):
-    """
-    Retorna la distancia más corta desde el origen hasta vertex.
-    """
-    vertex_info = mp.get(dijkstra_struct["visited"], vertex)
-    if vertex_info is None:
-        return math.inf
-    return vertex_info["dist_to"]
+def dist_to(search, vertex):
+    v_info = map.get(search["visited"], vertex)
+    return v_info["dist_to"]
 
 
-def path_to(vertex, dijkstra_struct):
-    """
-    Retorna una pila con el camino más corto desde el origen hasta vertex.
-    Cada elemento de la pila es un arco (edge).
-    """
-    # Si no hay camino, retornar None
-    if not has_path_to(dijkstra_struct, vertex):
+def path_to(search, vertex):
+    if not has_path_to(search, vertex):
         return None
 
-    # Crear pila vacia
     path = stack.new_stack()
-
     current = vertex
-    source = dijkstra_struct["source"]
-    
-    # Reconstruir el camino desde vertex hasta source
+    source = search["source"]
+    visited = search["visited"]
+
     while current != source:
-        vertex_info = mp.get(dijkstra_struct["visited"], current)
-        edge = vertex_info["edge_to"]
-        
+        v_info = map.get(visited, current)
+        edge = v_info["edge_from"]
         if edge is None:
             return None
-        
         stack.push(path, edge)
         current = edge["vertexA"]
 
